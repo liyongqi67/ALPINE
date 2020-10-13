2264ad876be8914cb051c0a56210b8174c9f0a7e
liyongqi <1083727181@qq.com>
2020-10-13 18:45:49 +0800

    Update precision.py

--- precision.py
+++ precision.py
@@ -11,11 +11,12 @@ for i in range(len(ground_truth)):
 
 
 
-
 for K in range(1,11):
-    recall=0
-    precision=0
-    f1=0
+    
+    recall=0.0
+    precision=0.0
+    f1=0.0
+
     for key in user_click:
         user_click[key].sort(key=lambda x:-x[1])
         
@@ -28,23 +29,28 @@ for K in range(1,11):
         for i in range(min(K*10,len(user_click[key]))):
             if(user_click[key][i][0]==1):
                 predicate_correct=predicate_correct+1
-        if(predicate_correct==0):
+        if(click_all==0 or predicate_correct==0):
             recall_temp=0
             precision_temp=0
-            f1_temp=0
+
         else:
             recall_temp=float(predicate_correct)/click_all
             precision_temp=float(predicate_correct)/min(K*10,len(user_click[key]))
-            f1_temp=2*(recall_temp*precision_temp)/(recall_temp+precision_temp)
+
+
+
+
+
         recall=recall+recall_temp
         precision=precision+precision_temp
-        f1=f1+f1_temp
+
 
     recall=recall/(len(user_click))
     precision=precision/(len(user_click))
-    f1=f1/(len(user_click))
+    f1=2*recall*precision/(recall+precision)
+    
 
-    print("K",K*10)
-    print('recall',recall)
-    print('precision',precision)
-    print('f1',f1)
+    print(K*10)
+    print(precision)
+    print(recall)
+    print(f1)
